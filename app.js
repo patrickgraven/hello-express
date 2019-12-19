93da619a95f43e9be012e64bb07d38daa5e09f1b
Pat Graven <patrick.graven@daugherty.com>
2019-12-19 13:47:31 -0600

    add error handling

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ app.get("/pjg", (req, res) => res.send("Hello PJG you!"));
 
 userEndpoints(app);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
